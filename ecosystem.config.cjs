cc11856607100a0e497acdedcc9b8895bb2b94b5
Juanes7222 <juan.cardona13@utp.edu.co>
2026-03-04 00:13:12 -0500

    Added config for vps setup

---
+++ ecosystem.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('pm2').StartOptions[]} */
+module.exports = {
+  apps: [
+    {
+      name: "radio-backend",
+      script: "./backend/dist/index.js",
+      cwd: "/var/www/radio",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+      env: {
+        NODE_ENV: "production",
+      },
+      env_file: "./backend/.env",
+      error_file: "/var/log/pm2/radio-backend-error.log",
+      out_file: "/var/log/pm2/radio-backend-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+    },
+  ],
+};
