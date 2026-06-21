c25de1afb55c3b7e482cb87bdd4e71d20e8ef520
Juanes7222 <juan.cardona13@utp.edu.co>
2026-06-21 15:44:06 -0500

    New metro config

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,16 +1,10 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
-const projectRoot = __dirname;
-const monorepoRoot = path.resolve(projectRoot, '../..');
+const config = getDefaultConfig(__dirname);
 
-const config = getDefaultConfig(projectRoot);
-
-config.watchFolders = [monorepoRoot];
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
+config.watchFolders = [
+  path.resolve(__dirname, '../..')
 ];
-config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
